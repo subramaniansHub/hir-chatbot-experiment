d4bd6ca708b3f0a3ccb6fb5bcdca952302531b0e
subramaniansHub <71336940+subramaniansHub@users.noreply.github.com>
2026-02-12 11:19:20 +0530

    Update generate_dummy_data.py

--- analysis/generate_dummy_data.py
+++ analysis/generate_dummy_data.py
@@ -33,7 +33,7 @@ def generate_dummy_data(n_per_condition=150):
     # Simulate Responses based on Condition (Effect Size)
     # High Flow should score higher on Flow-related questions and Outcomes
     
-    def simulate_likert(condition, high_mean, low_mean, sd=1.0):
+    def simulate_likert(condition, high_mean, low_mean, sd=0.8):
         if 'High' in condition:
             return np.clip(np.random.normal(high_mean, sd), 1, 5).round().astype(int)
         else:
@@ -51,6 +51,8 @@ def generate_dummy_data(n_per_condition=150):
     df_flow['perceived_warmth'] = df_flow['condition'].apply(lambda c: simulate_likert(c, 4.0, 2.2))
     df_flow['perceived_intelligence'] = df_flow['condition'].apply(lambda c: simulate_likert(c, 3.9, 3.1))
     df_flow['satisfaction'] = df_flow['condition'].apply(lambda c: simulate_likert(c, 4.1, 2.3))
+    df_flow['recommend_intention'] = df_flow['condition'].apply(lambda c: simulate_likert(c, 4.0, 2.6))
+
 
 
     # --- Experiment 2: Psychological Ownership ---
@@ -67,12 +69,17 @@ def generate_dummy_data(n_per_condition=150):
     df_own['own_choice'] = df_own['condition'].apply(lambda c: simulate_likert(c, 4.1, 2.6))
     df_own['own_decision'] = df_own['condition'].apply(lambda c: simulate_likert(c, 4.0, 2.4))
     df_own['own_control'] = df_own['condition'].apply(lambda c: simulate_likert(c, 3.9, 2.5))
+    df_own['own_personal'] = df_own['condition'].apply(lambda c: simulate_likert(c, 4.2, 2.7))
+    df_own['own_identity'] = df_own['condition'].apply(lambda c: simulate_likert(c, 4.0, 2.6))
+
 
     # Outcomes
     df_own['purchase_intention'] = df_own['condition'].apply(lambda c: simulate_likert(c, 3.9, 2.7))
     df_own['perceived_warmth'] = df_own['condition'].apply(lambda c: simulate_likert(c, 3.7, 3.0)) # Maybe less effect on warmth
     df_own['perceived_intelligence'] = df_own['condition'].apply(lambda c: simulate_likert(c, 4.0, 3.5))
     df_own['satisfaction'] = df_own['condition'].apply(lambda c: simulate_likert(c, 4.0, 2.8))
+    df_own['recommend_intention'] = df_own['condition'].apply(lambda c: simulate_likert(c, 4.1, 2.9))
+
 
     # Save to CSV
     df_flow.to_csv('experiment_flow_dummy.csv', index=False)
@@ -83,3 +90,4 @@ def generate_dummy_data(n_per_condition=150):
 
 if __name__ == "__main__":
     generate_dummy_data()
+
