713d80d768c9e23def0ca3b1420458cbc84a60f5
subramaniansHub <71336940+subramaniansHub@users.noreply.github.com>
2026-02-13 19:26:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -398,9 +398,11 @@ elif page != "survey":
              if current_step_name == "recommendation":
                  if st.button("Proceed to Survey", type="primary"):
                      st.session_state['chat_finished'] = True
+                     st.session_state['responses_submitted'] = False
                      # st.session_state['page_key'] = str(time.time())   # force new layout
-                     st.query_params["page"] = "survey"
-                     st.rerun()
+                     # st.query_params["page"] = "survey"
+                     st.switch_page("utils/survey.py")
+                     # st.rerun()
                      
              # --- NORMAL CHAT INPUT (ONLY WHILE CHAT IS ACTIVE) ---
              elif not st.session_state.get('chat_finished', False):
@@ -430,163 +432,6 @@ elif page != "survey":
         
                      next_step()
                      st.rerun()
-
-
-# --- SECTION 3: SURVEY ---
-else:
-    st.markdown(
-        """
-        <div id="survey_top"></div>
-        <script>
-            setTimeout(function() {
-                const el = document.getElementById("survey_top");
-                if (el) {
-                    el.scrollIntoView({behavior: "instant", block: "start"});
-                }
-            }, 150);
-        </script>
-        """,
-        unsafe_allow_html=True
-        )
-    page_key = st.session_state.get("page_key", "default")
-
-    with st.container(key=page_key):
-        st.title("Post-Interaction Survey")
-        st.write("Thank you for chatting! Please answer the following questions.")
-        st.divider()
-
-      
-        with st.form("survey_form"):
-    
-            response_data = {}
-            st.info("About You")
-            for q in DEMOGRAPHICS:
-                if q['type'] == 'number':
-                    response_data[q['id']] = st.number_input(q['question'], min_value=q.get('min_value', 0))
-                elif q['type'] == 'select':
-                    response_data[q['id']] = st.selectbox(q['question'], q['options'])
-                elif q['type'] == 'slider':
-                    # response_data[q['id']] = st.slider(q['question'], q['min'], q['max'], help=f"1={q['labels'][0]}, {q['max']}={q['labels'][1]}")
-                    
-                    response_data[q['id']] = st.slider(q['question'], q['min'], q['max'])
-                    # scale anchors under the slider
-                    st.markdown(
-                        f"<div style='display:flex; justify-content:space-between; font-size:13px;  margin-bottom:50px;'>"
-                        f"<span>{q['min']} ({q['labels'][0]})</span>"
-                        f"<span>{q['max']} ({q['labels'][1]})</span>"
-                        f"</div>",
-                        unsafe_allow_html=True
-                    )
-    
-    
-            st.markdown("---")
-            st.info("Your experience of the interaction")
-            
-            if st.session_state['experiment_group'] == 'Flow':
-                questions = FLOW_QUESTIONS
-            else:
-                questions = OWNERSHIP_QUESTIONS
-                
-            for q in questions:
-                #response_data[q['id']] = st.slider(q['question'], 1, 5, key=q['id'], help="1=Strongly Disagree, 5=Strongly Agree")
-                response_data[q['id']] = st.slider(q['question'], 1, 5, key=q['id'])
-                st.markdown(
-                    "<div style='display:flex; justify-content:space-between; font-size:13px;  margin-bottom:50px;'>"
-                    "<span>1 (Strongly Disagree)</span>"
-                    "<span>5 (Strongly Agree)</span>"
-                    "</div>",
-                    unsafe_allow_html=True
-                )
-    
-    
-    
-            st.markdown("---")
-            st.info("Final Thoughts")
-            for q in OUTCOMES:
-                 # response_data[q['id']] = st.slider(q['question'], 1, 5, key=q['id'], help="1=Strongly Disagree, 5=Strongly Agree")
-                response_data[q['id']] = st.slider(q['question'], 1, 5, key=q['id'])
-                st.markdown(
-                    "<div style='display:flex; justify-content:space-between; font-size:13px;  margin-bottom:50px;'>"
-                    "<span>1 (Strongly Disagree)</span>"
-                    "<span>5 (Strongly Agree)</span>"
-                    "</div>",
-                    unsafe_allow_html=True
-                )
-    
-    
-            # submitted = st.form_submit_button("Submit Responses")
-            submitted = st.form_submit_button("Submit Responses",disabled=st.session_state['responses_submitted'])
-            ### FORM ENDS HERE
-        
-            if submitted and not st.session_state['responses_submitted']:
-                #save_response()
-                save_response(
-                    experiment_name=f"Experiment {st.session_state['experiment_group']}",
-                    condition=st.session_state['full_condition'],
-                    data_dict=response_data
-                )
-                st.session_state['responses_submitted'] = True
-                # st.success("Thank you! Your responses have been recorded.")
-                # Force refresh
-                st.rerun()
-                ### FORM ENDS HERE
-
-if st.session_state['responses_submitted']:
-        st.success("Thank you! Your responses have been recorded.")
-
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
